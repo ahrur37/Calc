12774cf49a9f94b58d7def2fae621e40487d4010
Артур Зайнутдинов <ahrur37@gmail.com>
2025-04-25 16:57:59 +0300

    Добавьте файлы проекта.

---
+++ Calc/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calc
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void operationButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void regularButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPoint_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEquals_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClearAll_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClearEntry_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
